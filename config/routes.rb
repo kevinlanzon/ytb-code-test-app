f75034b0eacf00fc2de994a3a1f40a744c46c6d8
Kevin Lanzon <kevinlanzon@gmail.com>
2015-10-13 18:01:12 +0100

    changes book routes to only create and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :books
+    resources :books, only: [:create, :destroy]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
