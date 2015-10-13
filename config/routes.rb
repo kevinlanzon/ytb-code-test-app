0916ea8e70b1cb36e5007cad8a5913468c1d6060
Kevin Lanzon <kevinlanzon@gmail.com>
2015-10-13 17:07:45 +0100

    adds routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
-    member do
-      post 'create_book'
-      delete 'destroy_book'
-    end
+    resources :books
   end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
